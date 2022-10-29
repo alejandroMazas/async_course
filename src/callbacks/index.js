58d41a6f1862286dcb06f1f2c6c6170a9d18c33d
Alejandro Mazas de Lizana <alejandro.mazasdelizana@gmail.com>
2022-10-29 14:10:30 +0200

    callbacks

--- src/callbacks/index.js
+++ src/callbacks/index.js
@@ -1,7 +1,19 @@
-const sum = (num1, num2,) => num1 + num2
+const sum = (num1, num2) => num1 + num2
+const mult = (num1, num2) => num1 * num2
 
 const calc = (num1, num2, callback) => {
-    const result = callback(num1, num2)
-    return console.log(`El resultado es: ${result}`)
+    return callback(num1, num2)
 }
 
+const result = calc(3, 5, mult)
+console.log(`El resultado es: ${result}`)
+
+setTimeout(function () {
+    console.log('Hola javascript')
+}, 2000)
+
+function gretting(name) {
+    console.log(`Hola ${name}`)
+}
+
+setTimeout(gretting, 3000, 'Alejandro')
